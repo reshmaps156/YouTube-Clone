56d8622ef9069dfb2af6fee0e9e7def6cb4bf50d
Reshma PS <reshmaps156@gmail.com>
2024-06-21 22:51:11 +0530

    Added media queries for responsive layout

--- src/components/Widgetarea.jsx
+++ src/components/Widgetarea.jsx
@@ -20,40 +20,42 @@ import SensorsOutlinedIcon from '@mui/icons-material/SensorsOutlined';
 import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
 import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 import DryCleaningOutlinedIcon from '@mui/icons-material/DryCleaningOutlined';
+import { Link } from 'react-router-dom';
 function Widgetarea() {
+       
     return (
         <>
-            <Sidebar>
+            <Sidebar className='sideBar'>
                 <Menu>
 
-                    <MenuItem className=''><HomeSharpIcon className='me-2' /> Home </MenuItem>
-                    <MenuItem> <img src="https://i.pinimg.com/originals/17/d2/18/17d21878c22fe49e7e4752eecaa36541.png" className='me-2' alt="" width={'30px'} />Shorts </MenuItem>
-                    <MenuItem><SubscriptionsOutlinedIcon className='me-2' /> Subscriptions </MenuItem>
+                    <MenuItem className=''><Link to={'/'} className='unStyle'><HomeSharpIcon className='me-2' /><span className='sideBarDetail'> Home</span> </Link></MenuItem>
+                    <MenuItem> <img src="https://i.pinimg.com/originals/17/d2/18/17d21878c22fe49e7e4752eecaa36541.png" className='me-2' alt="" width={'30px'} /><span className='sideBarDetail'>Shorts</span> </MenuItem>
+                    <MenuItem><SubscriptionsOutlinedIcon className='me-2' /> <span className='sideBarDetail'>Subscriptions</span> </MenuItem>
                     <div className='seperation ms-3 m-2'></div>
-                    <SubMenu label="You" className='fw-bolder'>
+                    <SubMenu label="You" className='fw-bolder sideBarDetail'>
                         {/* <MenuItem> Pie charts </MenuItem>
                         <MenuItem> Line charts </MenuItem> */}
                     </SubMenu>
-                    <MenuItem ><SwitchAccountOutlinedIcon className='me-3'/> Your channel</MenuItem>
-                    <MenuItem><RestoreOutlinedIcon className='me-3'/> History </MenuItem>
-                    <MenuItem><PlaylistPlayOutlinedIcon className='me-3'/> Playlists</MenuItem>
-                    <MenuItem > <SlideshowOutlinedIcon className='me-3'/>Your videos</MenuItem>
-                    <MenuItem> <ScheduleOutlinedIcon className='me-3'/>Watch later </MenuItem>
-                    <MenuItem><ThumbUpOffAltOutlinedIcon className='me-3'/> Liked videos </MenuItem>
-                    <p className='ms-3 mt-3 fw-bold '>Subscriptions</p>
+                    <MenuItem ><SwitchAccountOutlinedIcon className='me-3'/> <span className='sideBarDetail'>Your channel</span></MenuItem>
+                    <MenuItem><Link to={'/history'} className='unStyle'><RestoreOutlinedIcon className='me-3'/><span className='sideBarDetail'> History </span></Link></MenuItem>
+                    <MenuItem><PlaylistPlayOutlinedIcon className='me-3'/><span className='sideBarDetail'> Playlists</span></MenuItem>
+                    <MenuItem > <SlideshowOutlinedIcon className='me-3'/><span className='sideBarDetail'>Your videos</span></MenuItem>
+                    <MenuItem> <ScheduleOutlinedIcon className='me-3'/><span className='sideBarDetail'>Watch later</span> </MenuItem>
+                    <MenuItem><ThumbUpOffAltOutlinedIcon className='me-3'/> <span className='sideBarDetail'>Liked videos</span> </MenuItem>
+                    <p className='ms-3 mt-3 fw-bold '><span className='sideBarDetail'>Subscriptions</span></p>
                     <div className='seperation ms-3 m-2'></div>
-                    <p className='ms-3 fw-bold '>Explore</p>
-                    <MenuItem ><WhatshotOutlinedIcon  className='me-3'/>Trending</MenuItem>
-                    <MenuItem><ShoppingBagOutlinedIcon  className='me-3'/> Shopping </MenuItem>
-                    <MenuItem><MusicNoteOutlinedIcon  className='me-3'/>Music</MenuItem>
-                    <MenuItem ><MovieCreationOutlinedIcon  className='me-3'/> Films</MenuItem>
-                    <MenuItem><SensorsOutlinedIcon  className='me-3'/> Live</MenuItem>
-                    <MenuItem><SportsEsportsOutlinedIcon  className='me-3'/> Gaming</MenuItem>
-                    <MenuItem><StickyNote2OutlinedIcon  className='me-3'/> News </MenuItem>
-                    <MenuItem><EmojiEventsOutlinedIcon  className='me-3'/> Sport</MenuItem>
-                    <MenuItem > <LightbulbOutlinedIcon  className='me-3'/>Courses</MenuItem>
-                    <MenuItem> <DryCleaningOutlinedIcon  className='me-3'/>Fashion & beauty </MenuItem>
-                    <MenuItem> <PodcastsOutlinedIcon  className='me-3'/>Podcasts </MenuItem>
+                    <p className='ms-3 fw-bold '><span className='sideBarDetail'><span className='sideBarDetail'>Explore</span></span></p>
+                    <MenuItem ><WhatshotOutlinedIcon  className='me-3'/><span className='sideBarDetail'>Trending</span></MenuItem>
+                    <MenuItem><ShoppingBagOutlinedIcon  className='me-3'/><span className='sideBarDetail'> Shopping </span></MenuItem>
+                    <MenuItem><MusicNoteOutlinedIcon  className='me-3'/><span className='sideBarDetail'>Music</span></MenuItem>
+                    <MenuItem ><MovieCreationOutlinedIcon  className='me-3'/> <span className='sideBarDetail'>Films</span></MenuItem>
+                    <MenuItem><SensorsOutlinedIcon  className='me-3'/> <span className='sideBarDetail'>Live</span></MenuItem>
+                    <MenuItem><SportsEsportsOutlinedIcon  className='me-3'/><span className='sideBarDetail'> Gaming</span></MenuItem>
+                    <MenuItem><StickyNote2OutlinedIcon  className='me-3'/><span className='sideBarDetail'> News </span></MenuItem>
+                    <MenuItem><EmojiEventsOutlinedIcon  className='me-3'/><span className='sideBarDetail'> Sport</span></MenuItem>
+                    <MenuItem > <LightbulbOutlinedIcon  className='me-3'/><span className='sideBarDetail'>Courses</span></MenuItem>
+                    <MenuItem> <DryCleaningOutlinedIcon  className='me-3'/><span className='sideBarDetail'>Fashion & beauty</span> </MenuItem>
+                    <MenuItem> <PodcastsOutlinedIcon  className='me-3'/><span className='sideBarDetail'>Podcasts</span> </MenuItem>
                 </Menu>
             </Sidebar>
         </>
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -38,29 +38,29 @@ function History() {
               <div className="row">
                 <div className="col-md-4">
                   <img src="https://i.pinimg.com/236x/43/f6/b2/43f6b246b2b832de3ea6e8a4a9a1a7f6.jpg" alt="" className='w-100 h-75 rounded' />
-                 <div className='d-flex mt-1 fw-bold'> <p>Lorem ipsum | dolor sit|  amet consectetur .....</p>
+                 <div className='d-flex mt-1 fw-bold'> <p className='shortsDescription'>Lorem ipsum | dolor sit|  amet consectetur .....</p>
                  <MoreVertIcon/>
                  </div>
                 </div>
                 <div className="col-md-4">
                   <img src="https://i.pinimg.com/originals/17/a7/40/17a74008413e523213faeac55e46fd2f.jpg" alt="" className='w-100 h-75 rounded' />
-                  <div className='d-flex mt-1 fw-bold'> <p>Lorem ipsum | dolor sit|  amet consectetur .....</p>
+                  <div className='d-flex mt-1 fw-bold'> <p  className='shortsDescription'>Lorem ipsum | dolor sit|  amet consectetur .....</p>
                  <MoreVertIcon/>
                  </div>
                 </div>
                 <div className="col-md-4">
                   <img src="https://miro.medium.com/v2/resize:fit:1080/1*rhKX-gQxcEOxovAt-djzuQ.jpeg" alt="" className='w-100 h-75 rounded' />
-                  <div className='d-flex mt-1 fw-bold'> <p>Lorem ipsum | dolor sit|  amet consectetur .....</p>
+                  <div className='d-flex mt-1 fw-bold'> <p  className='shortsDescription'>Lorem ipsum | dolor sit|  amet consectetur .....</p>
                  <MoreVertIcon/>
                  </div>
                 </div>
               </div>
-              <div className="row">
+              <div className="row ">
                 <div className="col-md-12 borderDiv shadow">
 
                 </div>
               </div>
-              <div className="row mt-2">
+              <div className="row mt-2 videoHistory">
                 <div className="col-md-4">
                   <iframe width="100%" height="" src="https://www.youtube.com/embed/__6sIqZk-DA?si=48FT2y774n3zwDey" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
@@ -70,7 +70,7 @@ function History() {
                   <p className='text-secondary smallText'>Baby Calm Down (FULL VIDEO SONG) | Selena Gomez & Rema Official Music Video 2023 | HD 4K</p>
                 </div>
                 <div className="col-md-1">
-                  <div className='d-flex'>
+                  <div className='d-flex clearHistoryBtn'>
                     <CloseOutlinedIcon />
                     <MoreVertIcon />
                   </div>
@@ -86,7 +86,7 @@ function History() {
                   <p className='text-secondary smallText'>Havana" feat. Young Thug  available at iTunes </p>
                 </div>
                 <div className="col-md-1">
-                  <div className='d-flex'>
+                  <div className='d-flex clearHistoryBtn'>
                     <CloseOutlinedIcon />
                     <MoreVertIcon />
                   </div>
@@ -102,7 +102,7 @@ function History() {
                   <p className='text-secondary smallText'>Señorita: https://Senorita.lnk.to/OutNow Catch Shawn on tour this year: https://www.shawnmendesthetour.com/ Stream/buy Shawn </p>
                 </div>
                 <div className="col-md-1">
-                  <div className='d-flex'>
+                  <div className='d-flex clearHistoryBtn'>
                     <CloseOutlinedIcon />
                     <MoreVertIcon />
                   </div>
@@ -111,12 +111,12 @@ function History() {
             </div>
             <div className="col-md-4 mt-3 p-5 ">
               <Input placeholder="Search watch history" inputProps={ariaLabel} className=' ' />
-              <p className='border-0 mt-3 bg-transparent '><FontAwesomeIcon icon={faTrashCan} className='ms-1 me-3' />Clear all watch history</p>
-              <p className='border-0 mt-3 bg-transparent '><PauseOutlinedIcon className='ms-1 me-3' />Pause watch history</p>
-              <p className='border-0 mt-3 bg-transparent '><SettingsOutlinedIcon className='ms-1 me-3' />Manage all history</p>
-              <p className='text-secondary mt-3 ms-5'>Comments</p>
-              <p className='text-secondary ms-5'>Posts</p>
-              <p className='text-secondary ms-5'>Live Chats</p>
+              <p className='border-0 mt-3 bg-transparent historySetFont'><FontAwesomeIcon icon={faTrashCan} className='ms-1 me-3' />Clear all watch history</p>
+              <p className='border-0 mt-3 bg-transparent historySetFont'><PauseOutlinedIcon className='ms-1 me-3' />Pause watch history</p>
+              <p className='border-0 mt-3 bg-transparent historySetFont'><SettingsOutlinedIcon className='ms-1 me-3' />Manage all history</p>
+              <p className='text-secondary mt-3 ms-5 historySetFont'>Comments</p>
+              <p className='text-secondary ms-5 historySetFont'>Posts</p>
+              <p className='text-secondary ms-5 historySetFont'>Live Chats</p>
             </div>
           </div>
 
